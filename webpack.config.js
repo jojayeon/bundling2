ea46a0c80a652c3505a9fcd7451a75a47be89552
jojayeon <jojayeon6152@gmail.com>
2024-07-09 10:47:49 +0900

    세팅

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  output: {
+    filename: "index.bundle.js",
+    path : path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      
+      {
+        test:/\.css$/,
+        use: ['style-loader','css-loader'],
+      },
+      {
+        test:/\.js|jsx|ts|tsx$/,
+        use:'babel-loader',
+        exclude:/node_modules/,
+      },
+      {
+        test:/\.(ts|tsx)$/,
+        use:'ts-loader',
+        exclude:/node_modules/,
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".ts", ".js", ".tsx", ".jsx", ".css"]
+  }
+}
